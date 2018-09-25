0012146780df62f76c3a7dd8ca29431eacf6da1a
CoolRuslanCool <cool3331982@mail.ru>
2018-09-25 19:22:51 +0300

    add Renderer2 for render elements

--- src/app/directives/text-color.directive.ts
+++ src/app/directives/text-color.directive.ts
@@ -1,16 +1,19 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, OnInit, Renderer2} from '@angular/core';
 
 @Directive({
   selector: '[appTextColor]'
 })
 export class TextColorDirective implements OnInit {
 
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit(): void {
-    const style = this.element.nativeElement.style;
-    style.color = 'red';
-    style.backgroundColor = 'darkcyan';
+    const nativeElement = this.element.nativeElement;
+    this.renderer.setStyle(nativeElement, 'color', 'blue');
+    this.renderer.setStyle(nativeElement, 'backgroundColor', 'red');
+    // const style = this.element.nativeElement.style;
+    // style.color = 'red';
+    // style.backgroundColor = 'darkcyan';
   }
 
 
