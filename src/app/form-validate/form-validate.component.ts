ab387fa50f867752e8f2d58c8303c650f8e6e8e1
CoolRuslanCool <cool3331982@mail.ru>
2018-10-02 02:10:47 +0300

    templateDriving validate form

--- src/app/form-validate/form-validate.component.ts
+++ src/app/form-validate/form-validate.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {NgForm} from '@angular/forms';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {FormControl, NgForm} from '@angular/forms';
 import {Logger} from '../services/app-logger-util';
 
 @Component({
@@ -9,13 +9,32 @@ import {Logger} from '../services/app-logger-util';
 })
 export class FormValidateComponent implements OnInit {
 
+  submitted = false;
+  @ViewChild('form')
+  form: NgForm;
+  data = {};
+
+  @ViewChild('email')
+  email: FormControl;
+
+  defEmail = 'def';
+
   constructor(private logger: Logger) { }
 
   ngOnInit() {
   }
 
   validateForm(form: NgForm) {
+    const {value, valid} = form;
+  // validateForm(form: NgForm) {
+  //   this.logger.log(value);
+  //   this.logger.log(valid);
+
+    this.logger.log(this.email);
     this.logger.log(form);
+    this.data = this.form.value.user;
+    this.submitted = true;
+
   }
 
 }
